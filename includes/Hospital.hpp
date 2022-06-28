8e12de92f9492baa6c6164c2d173886f5922c8c7
Samael <jeremie.joannard@epitech.eu>
2022-06-29 01:14:52 +0200

    [ADD] includes/, [ADD] sources/Classes/,

---
+++ includes/Hospital.hpp
@@ -0,0 +1,20 @@
+/*
+** EPITECH PROJECT, 2022
+** Untitled (Workspace)
+** File description:
+** Hospital
+*/
+
+#ifndef HOSPITAL_HPP_
+#define HOSPITAL_HPP_
+
+class Hospital {
+    public:
+        Hospital();
+        ~Hospital();
+
+    protected:
+    private:
+};
+
+#endif /* !HOSPITAL_HPP_ */
---
+++ sources/Classes/Hospital.cpp
@@ -0,0 +1,16 @@
+/*
+** EPITECH PROJECT, 2022
+** Untitled (Workspace)
+** File description:
+** Hospital
+*/
+
+#include "Hospital.hpp"
+
+Hospital::Hospital()
+{
+}
+
+Hospital::~Hospital()
+{
+}
